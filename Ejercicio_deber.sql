4ebeb788c981732b06346cab90d61d2770306b22
IDIC <alumnos@alumnosuio.ute.edu.ec>
2026-04-21 08:57:11 -0500

    Ejercicio deber

--- Ejercicio_deber.sql
+++ Ejercicio_deber.sql
@@ -1,65 +1,7 @@
 /* Nombre: Roberto Rochaalter 
-Fecha: 16/04/2026
-Tema: Ejercio en clase IF/CASE
+Fecha: 21/04/2026
+Tema: Ejercio calculo impuesto predial
 Docente: Ing.Christian Rivadeneira*/
-
-
-/*Ejercicio if - case
- *  >10 frio
- * 10 - 25 templado
- * 25 calor
- */
-
-create or replace function fn_claificacion_temperatura(temperatura int) returns text as $$
-begin
-	if temperatura < 10 then
-		return 'Frio';
-	elseif temperatura between 10 and 25 then
-		return 'Templado';
-	else
-		return 'Calor';
-	end if;
-end ;
-$$ language plpgsql
-
-
-
-create table tbl_temperatura(
-id serial primary key,
-temperatura int
-);
-
-
-insert into tbl_temperatura(temperatura) values
-(20),
-(5),
-(28),
-(7),
-(22)
-
-
--- case
-create or replace function fn_claificacion_temperatura_case(temperatura int) returns text as $$
-begin
-	return case 
-			when temperatura < 10 then 'Frio'
-			when temperatura between 10 and 25 then'Templado'
-			else 'Calor'
-			end;
-end ;
-$$ language plpgsql
-
-
-select
-temperatura,
-fn_claificacion_temperatura(temperatura)
-from tbl_temperatura
-
-
-select
-temperatura,
-fn_claificacion_temperatura_case(temperatura)
-from tbl_temperatura
 
 
 /*Pago impuesto predial
